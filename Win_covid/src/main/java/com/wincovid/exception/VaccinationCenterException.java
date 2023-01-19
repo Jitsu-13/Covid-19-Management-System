4f7a46dfe7e299944b12dc00cf6b1d60d034d5e5
Jitsu-13 <yadav_sujit@outlook.com>
2023-01-19 10:21:26 +0530

    Appoint and Vaccination Service

---
+++ Win_covid/src/main/java/com/wincovid/exception/VaccinationCenterException.java
@@ -0,0 +1,15 @@
+package com.wincovid.exception;
+
+public class VaccinationCenterException extends Exception{
+
+	public VaccinationCenterException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public VaccinationCenterException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+}
